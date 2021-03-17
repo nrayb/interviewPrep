73d2053baacdd3ef389c865ae7ddaf727f1d6227
Noel Ray Barron <noelraybarron@hotmail.com>
2021-03-16 21:57:54 -0700

    Write tests, corrections, and notes for 2dot1

--- Typescript/src/2dot1.ts
+++ Typescript/src/2dot1.ts
@@ -1,9 +1,9 @@
 import { LinkedListNode } from "./2types";
 
-export function removeDumplicate(firstNode: LinkedListNode) {
+export function removeDumplicate(firstNode?: LinkedListNode) {
     const appearanceMap: {[key: number]: boolean} = {};
     let prev: LinkedListNode = { value: 0, next: firstNode };
-    while (prev.next !== null) {
+    while (prev.next) {
         if (appearanceMap[prev.next.value]) {
             prev.next = prev.next.next;
         } else {
@@ -14,17 +14,40 @@ export function removeDumplicate(firstNode: LinkedListNode) {
 
 }
 
-export function removeDumplicateWithoutBuffer(firstNode: LinkedListNode) {
+export function removeDumplicateWithoutBuffer(firstNode?: LinkedListNode) {
     let currNode = firstNode;
-    while (currNode.next !== null) {
+    while (currNode && currNode.next) {
         let node2 = currNode;
-        while (node2.next !== null) {
+        while (node2.next) {
             if (node2.next.value === currNode.value) {
                 node2.next = node2.next.next;
             } else {
                 node2 = node2.next;
             }
         }
-        currNode.next = currNode.next.next;
+        currNode = currNode.next;
     }
 }
+
+/**
+ * Notes for myself in terms of what I need to do better after writing the tests
+ * 1. Test edge cases better
+ *      - I didn't catch the case that the node that was passed in was undefined
+ *      - I also didn't catch the case that the node has repeating values.
+ * 2. Think back to more language specific features
+ *      - I made a mistake doing "currNode.next !== null"
+ *      - I should have known better not to do that and double check what the end of a LL looked like
+ *      - In my defined interface, a LL terminated at `undefined` which is `!== null`
+ * 3. Ask better questions
+ *      - I made an assumption in the verry begining that the input would be undefined
+ *      - I should verify things like these right away
+ * 4. I THINK MOST IMPORTANTLY, don't half-ass your practice
+ *      - I did relatively well on the first part, but kind of bombed the second part....
+ *      - I was half-assing it and clearly it showed as I wrote the tests.
+ *
+ * Good questions to start up with:
+ * 1. Can I assume that the input will always be defined?
+ * 2. What do you want the function to return?
+ *      - This would be a good time to flex a little bit of knowledge about functional vs OO programming
+ * 3. Validate what the interface looks like.
+ */
--- Typescript/test/2dot1.test.ts
+++ Typescript/test/2dot1.test.ts
@@ -7,7 +7,7 @@ import {
 function validateNoDuplicates(firstNode: LinkedListNode): boolean {
     const appearanceMap: {[key: number]: boolean} = {};
     const prevNode: LinkedListNode = { value: 0, next: firstNode };
-    while (prevNode.next !== null) {
+    while (prevNode.next) {
         if (appearanceMap[prevNode.next.value]) {
             return false;
         } else {
@@ -19,6 +19,17 @@ function validateNoDuplicates(firstNode: LinkedListNode): boolean {
 }
 
 describe("removeDumplicate", () => {
+    test(" tested with no input", () => {
+        expect(() => removeDumplicate()).not.toThrow();
+    });
+
+    test(" tested with one node (1)", () => {
+        const testNode: LinkedListNode = {
+            value: 1
+        };
+        expect(() => removeDumplicate(testNode)).not.toThrow();
+    });
+
     test(" tested with input 1-1-1", () => {
         const testNode: LinkedListNode = {
             value: 1,
@@ -30,12 +41,55 @@ describe("removeDumplicate", () => {
             }
         };
         removeDumplicate(testNode);
-        expect(testNode.next).toBeFalsey();
+        expect(testNode.next).toBeFalsy();
+        expect(validateNoDuplicates(testNode)).toBeTruthy();
+    });
+
+    test(" tested with input 1-2-1-2-3-4-5", () => {
+        const testNode: LinkedListNode = {
+            value: 1,
+            next: {
+                value: 2,
+                next: {
+                    value: 1,
+                    next: {
+                        value: 2,
+                        next: {
+                            value: 3,
+                            next: {
+                                value: 4,
+                                next: {
+                                    value: 5,
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        removeDumplicate(testNode);
+        expect(testNode.value).toEqual(1);
+        expect(testNode.next.value).toEqual(2);
+        expect(testNode.next.next.value).toEqual(3);
+        expect(testNode.next.next.next.value).toEqual(4);
+        expect(testNode.next.next.next.next.value).toEqual(5);
+        expect(testNode.next.next.next.next.next).toBeFalsy();
         expect(validateNoDuplicates(testNode)).toBeTruthy();
     });
 });
 
 describe("removeDumplicateWithoutBuffer", () => {
+    test(" tested with no input", () => {
+        expect(() => removeDumplicateWithoutBuffer()).not.toThrow();
+    });
+
+    test(" tested with one node (1)", () => {
+        const testNode: LinkedListNode = {
+            value: 1
+        };
+        expect(() => removeDumplicateWithoutBuffer(testNode)).not.toThrow();
+    });
+
     test(" tested with input 1-1-1", () => {
         const testNode: LinkedListNode = {
             value: 1,
@@ -47,7 +101,39 @@ describe("removeDumplicateWithoutBuffer", () => {
             }
         };
         removeDumplicateWithoutBuffer(testNode);
-        expect(testNode.next).toBeFalsey();
+        expect(testNode.next).toBeFalsy();
+        expect(validateNoDuplicates(testNode)).toBeTruthy();
+    });
+
+    test(" tested with input 1-2-1-2-3-4-5", () => {
+        const testNode: LinkedListNode = {
+            value: 1,
+            next: {
+                value: 2,
+                next: {
+                    value: 1,
+                    next: {
+                        value: 2,
+                        next: {
+                            value: 3,
+                            next: {
+                                value: 4,
+                                next: {
+                                    value: 5,
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        removeDumplicateWithoutBuffer(testNode);
+        expect(testNode.value).toEqual(1);
+        expect(testNode.next.value).toEqual(2);
+        expect(testNode.next.next.value).toEqual(3);
+        expect(testNode.next.next.next.value).toEqual(4);
+        expect(testNode.next.next.next.next.value).toEqual(5);
+        expect(testNode.next.next.next.next.next).toBeFalsy();
         expect(validateNoDuplicates(testNode)).toBeTruthy();
     });
 });
