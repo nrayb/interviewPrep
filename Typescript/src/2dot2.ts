b7c5d0deea2058f7907f698e7f45ed5b8e6dfc93
Barron, Noel Ray <noel.ray.barron@sap.com>
2021-03-19 17:46:58 -0700

    Add some more notes and experiments about recursion in LL

--- Typescript/src/2dot2.ts
+++ Typescript/src/2dot2.ts
@@ -77,3 +77,25 @@ export function getKthElementInLinkedList(firstNode: LinkedListNode | undefined,
   *   - Be especially careful when there are two inputs, and you cover all bad cases
   *   - i.e. (bad1, good2) (good1, bad2) (bad1, bad2)
   */
+
+// So after writing this, I looked into the solution in the book, and their default solution was recursive.
+// Since I'm not very good with recursion, I think this would be a good way to visit them
+
+// Let's first limit our exposure to recursion to the scope of linkedLists.
+// When traversing through a linked list through recusion, in general we do something like this:
+export function recursivelyTraverseThroughEntireLinkedList(head: LinkedListNode) {
+  if (head.next) {
+    recursivelyTraverseThroughEntireLinkedList(head.next);
+  }
+}
+
+// So now if we want to let's say add all the values in a linkedList, we then do something like this:
+export function addAllValuesInLinkedListRecursively(head: LinkedListNode | undefined): number {
+  if (!head) {
+    return 0;
+  }
+  return head.value + addAllValuesInLinkedListRecursively(head.next);
+}
+// So when thinking about this solution, I had to be very careful to make sure I consider the edge values and that I process them properly.
+// My first solution only processed the head.next value which meant that we wouldn't have gotten the value of the very head of the LinkedList
+// Okay so recursion in LinkedLists is actually not too bad.
